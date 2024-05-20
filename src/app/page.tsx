f39c7a79b22f8e6378245bb0c60dd431b7b0d165
chulinuwuw <ananzxzoxd@gmail.com>
2024-05-21 01:17:19 +0700

    Initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import circleImg from "../../public/img/circle.png";
+import pekoraImg from "../../public/img/pekora.png";
+import 'animate.css';
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image className="absolute w-[600px] h-[600px] " src={circleImg} alt={"circle"} width={0} height={0}>
+      <Image className="absolute lg:w-[600px] lg:h-[600px] sm:w-[70px] sm:h-[200px]" src={circleImg} alt={"circle"} width={0} height={0}>
         
       </Image>
+      <Image
+        src={pekoraImg}
+        alt="peko"
+        width={0}
+        height={0}
+        className="absolute w-[718px] h-auto animate__animated animate__bounce animate__delay-1s"></Image>
     </main>
   );
 }
@@ -22,3 +29,28 @@ export default function Home() {
 
 // background: #FFFFFF;
 // box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+
+/* Ellipse 1 */
+
+// position: absolute;
+// width: 600px;
+// height: 600px;
+// left: 420px;
+// top: 212px;
+
+// background: #FFFFFF;
+// box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+
+/* Usada-Pekora_pr-img_05-718x1440 1 */
+
+// position: absolute;
+// width: 718px;
+// height: 1440px;
+// left: 353px;
+// top: 159px;
+
+// background: url(Usada-Pekora_pr-img_05-718x1440.png);
+// filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,42 @@
-import React from 'react';
+'use client'
+import React, { useState, useEffect } from 'react';
 
 export default function navbar() {
+
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [navbarHeight, setNavbarHeight] = useState('h-20');
+  const [textSize, setTextSize] = useState('text-2xl');
+  
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const show = window.scrollY > 0;
+      if (show) {
+        setIsScrolled(true);
+        setNavbarHeight('h-16');
+        setTextSize('text-xl');
+      } else {
+        setIsScrolled(false);
+        setNavbarHeight('h-20');
+        setTextSize('text-2xl');
+      }
+    };
+
+    document.addEventListener('scroll', handleScroll);
+    return () => {
+      document.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div>
+  
       
 
-<nav className="bg-white border-gray-200 dark:bg-gray-900">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+<nav className={`fixed top-0 w-[100%] shadow-md z-50 transition-all duration-500 ${isScrolled ? 'bg-white' : 'bg-transparent'} ${navbarHeight} border-gray-200 dark:bg-gray-900`}>
+  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 h-[100%]">
+    <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse text-red-600">
+        {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" /> */}
+        <span className={`font-semibold whitespace-nowrap dark:text-white text-4xl`}>Chulinxz</span>
     </a>
     <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
@@ -18,27 +45,26 @@ export default function navbar() {
         </svg>
     </button>
     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
-          <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+          <a href="#" className="text-xl block py-2 px-3 text-white bg-red-300 rounded md:bg-transparent md:text-red-500 md:p-0 dark:text-white md:dark:text-red-500" aria-current="page">Home</a>
         </li>
         <li>
-          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+          <a href="#" className="text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
         </li>
         <li>
-          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+          <a href="#" className="text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
         </li>
         <li>
-          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+          <a href="#" className="text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
         </li>
         <li>
-          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+          <a href="#" className="text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
         </li>
       </ul>
     </div>
   </div>
 </nav>
 
-    </div>
   );
 }
